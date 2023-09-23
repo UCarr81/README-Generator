f52aedba9bd1918b6a9c608e562b78f88502ed37
Uriel <CarrilloUriel81@gmail.com>
2023-09-22 22:21:14 -0700

    Added code so It genereates a README file now

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,36 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer')
+const generateReadme = require('./utils/generateMarkdowns')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+
+    }
 
 // TODO: Create a function to initialize app
-function init() {}
+
+function init() {
+
+    inquirer.prompt(questions).then((response) => {
+        const readmeFile = generateReadme(response); // Use generateMarkdown here
+        writeToFile('README.md', readmeFile);
+        console.log('README.md File Generated');
+    })
+}
 
 // Function call to initialize app
+
 init();
