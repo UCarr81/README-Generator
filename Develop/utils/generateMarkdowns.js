54ad9573f7d48b0c2b8e6d9f7be61bec315eb063
Uriel <CarrilloUriel81@gmail.com>
2023-09-24 16:24:27 -0700

    Debug'd Code in Github Username and fixed infinite loop hole in License sections and liks

--- Develop/utils/generateMarkdowns.js
+++ Develop/utils/generateMarkdowns.js
@@ -66,7 +66,7 @@ switch(license){
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  let stringLicenseSection = ''
+  let stringLicenseSection = '';
 
   switch(license){
     case 'Apache':
@@ -91,12 +91,14 @@ function renderLicenseSection(license) {
       break;
   }
 
+  return stringLicenseSection;
+
   stringLicenseSection += renderLicenseSection(license);
   return stringLicenseSection;
 }
 
 function githubUsername(userName){
-  if (username != ''){
+  if (userName != ''){
     return 'https://github.com'+userName;
   }else{
     return '';
