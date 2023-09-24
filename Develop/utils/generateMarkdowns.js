a072a313b1b40a6a83045a566c9cdb4041c58912
Uriel <CarrilloUriel81@gmail.com>
2023-09-24 16:16:56 -0700

    updated License links

--- Develop/utils/generateMarkdowns.js
+++ Develop/utils/generateMarkdowns.js
@@ -27,20 +27,69 @@ function renderLicenseBadge(license) {
     case 'Unlicense':
       return '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
       break;
-
-    default: 
-      return '';
-      break;
   } 
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+switch(license){
+
+  case 'Apache':
+    return '[https://opensource.org/licenses/Apache-2.0](https://opensource.org/licenses/Apache-2.0)';
+    break;
+
+  case 'Boost':
+    return '[https://www.boost.org/LICENSE_1_0.txt](https://www.boost.org/LICENSE_1_0.txt)';
+    break;
+
+  case 'MIT':
+    return '[https://opensource.org/licenses/MIT](https://opensource.org/licenses/MIT)';
+    break;
+
+  case 'Mozilla':
+    return '[https://opensource.org/licenses/MPL-2.0](https://opensource.org/licenses/MPL-2.0)';
+    break;
+
+  case 'IBM':
+    return '[https://opensource.org/licenses/IPL-1.0](https://opensource.org/licenses/IPL-1.0)';
+    break;
+
+  case 'Unlicense':
+    return '[http://unlicense.org/](http://unlicense.org/)';
+    break;
+}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let stringLicenseSection = ''
+
+  switch(license){
+    case 'Apache':
+      stringLicenseSection += '## Apache\n' + '### Apache 2.0 License';
+      break;
+      case 'Boost':
+      stringLicenseSection += '## Boost\n' + '### Boost Software License 1.0';
+      break;
+      case 'MIT':
+      stringLicenseSection += '## MIT\n' + '### The MIT License';
+      break;
+      case 'Mozilla':
+      stringLicenseSection += '## Mozilla\n' + '### Mozilla Public License 2.0';
+      break;
+      case 'IBM':
+      stringLicenseSection += '## IBM\n' + '### Mozilla Public License 2.0';
+      break;
+      case 'Unlicense':
+      stringLicenseSection += '## Unlicense\n' + '### The Unlicense';
+      break;
+  }
+
+  stringLicenseSection += renderLicenseSection(license);
+  return stringLicenseSection;
+}
 
 function githubUsername(userName){
   if (username != ''){
@@ -102,7 +151,8 @@ ${data.test}`;
   if (data.license) {
     markdown += `
 ## License
-${data.license}`;
+${renderLicenseSection(data.license)}
+${renderLicenseLink(data.license)}`;
   }
   markdown += `
 
